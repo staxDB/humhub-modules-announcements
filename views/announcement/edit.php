72223db78f037822b6bb91e81b3b0374461240e8
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-02-01 13:32:17 +0100

    Update edit.php

--- views/announcement/edit.php
+++ views/announcement/edit.php
@@ -1,6 +1,9 @@
 <?php
 
 use humhub\widgets\ActiveForm;
+use humhub\widgets\RichtextField;
+use humhub\modules\file\widgets\FilePreview;
+use humhub\modules\file\widgets\UploadButton;
 use yii\helpers\Html;
 
 /* @var $announcement \humhub\modules\announcements\models\Announcement */
@@ -10,50 +13,45 @@ use yii\helpers\Html;
 ?>
 
 <div class="content_edit input-container" id="comment_edit_<?= $announcement->id; ?>" data-message="<?= $announcement->id ?>" data-content-component="announcements.Message" data-content-key="<?= $announcement->content->id ?>" >
-    <div  class="alert alert-danger" role="alert" style="display:none">
+    <div class="alert alert-danger" role="alert" style="display:none">
         <span class="errorMessage"></span>
     </div>
 
     <?php $form = ActiveForm::begin(); ?>
 
-
-    <?= \humhub\widgets\RichtextField::widget([
+    <?= RichtextField::widget([
         'form' => $form,
         'model' => $announcement,
         'attribute' => 'message',
         'disabled' => $announcement->closed,
         'placeholder' => Yii::t('AnnouncementsModule.base', 'Edit your message')
-    ]); ?>
-
+    ]);
+    ?>
 
     <div class="comment-buttons">
-
-        <?=
-        \humhub\modules\file\widgets\UploadButton::widget([
+        <?= UploadButton::widget([
             'id' => 'announcement_upload_' . $announcement->id,
             'model' => $announcement,
-            'dropZone' => '#announcement_'.$announcement->id,
-            'preview' => '#announcement_upload_preview_'.$announcement->id,
-            'progress' => '#announcement_upload_progress_'.$announcement->id,
+            'dropZone' => '#announcement_' . $announcement->id,
+            'preview' => '#announcement_upload_preview_' . $announcement->id,
+            'progress' => '#announcement_upload_progress_' . $announcement->id,
             'max' => Yii::$app->getModule('content')->maxAttachedFiles
         ]);
         ?>
 
-    <a href="#" class="btn btn-default btn-sm btn-comment-submit"
-       data-action-click="editSubmit"
-       data-action-url="<?= $announcement->content->container->createUrl('/announcements/announcement/edit', ['id' => $announcement->id]) ?>"
-       data-action-submit
-       data-ui-loader>
-        <?= Yii::t('AnnouncementsModule.views', "Save") ?>
-    </a>
-
+        <a href="#" class="btn btn-default btn-sm btn-comment-submit"
+            data-action-click="editSubmit"
+            data-action-url="<?= $announcement->content->container->createUrl('/announcements/announcement/edit', ['id' => $announcement->id]) ?>"
+            data-action-submit
+            data-ui-loader>
+                <?= Yii::t('AnnouncementsModule.views', 'Save') ?>
+        </a>
     </div>
 
     <div id="announcement_upload_progress_<?= $announcement->id ?>" style="display:none; margin:10px 0;"></div>
 
-    <?=
-    \humhub\modules\file\widgets\FilePreview::widget([
-        'id' => 'announcement_upload_preview_'.$announcement->id,
+    <?= FilePreview::widget([
+        'id' => 'announcement_upload_preview_' . $announcement->id,
         'options' => ['style' => 'margin-top:10px'],
         'model' => $announcement,
         'edit' => true
