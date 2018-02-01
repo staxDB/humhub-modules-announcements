16a09c03fa0d47a9233cb12c602fa450bb879c74
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-02-01 13:57:49 +0100

    Update form.php

--- widgets/views/form.php
+++ widgets/views/form.php
@@ -6,15 +6,14 @@ use humhub\modules\announcements\assets\AnnouncementsAsset;
 use humhub\widgets\RichtextField;
 
 AnnouncementsAsset::register($this);
-?>
 
-<?= RichtextField::widget([
+echo RichtextField::widget([
     'name' => 'message',
     'placeholder' => Yii::t('AnnouncementsModule.widgets', 'Add Announcement...'),
-]); ?>
+]);
+?>
 
-
-<!--// show hints-->
+<!-- Show hints -->
 <div class="contentForm_options" data-content-component="announcements.Announcement">
     <p class="help-block"><?= Yii::t('AnnouncementsModule.widgets', 'Note: You can use markdown syntax. (For more information visit <a target="_blank" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">this link</a>)'); ?></p>
 </div>
