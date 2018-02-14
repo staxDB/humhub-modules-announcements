1d011d052c3849341418018454111b4f55a45f93
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-02-14 11:00:44 +0100

    Update m171208_130303_initial.php

--- migrations/m171208_130303_initial.php
+++ migrations/m171208_130303_initial.php
@@ -6,21 +6,21 @@ class m171208_130303_initial extends Migration
 {
     public function up()
     {
-        $this->createTable('announcement', array(
+        $this->createTable('announcement', [
             'id' => 'pk',
             'message' => 'TEXT NULL',
             'closed'=> 'tinyint(4) DEFAULT 0'
-        ), '');
+        ], '');
 
-        $this->createTable('announcement_user', array(
+        $this->createTable('announcement_user', [
             'id' => 'pk',
             'announcement_id' => 'int(11) NOT NULL',
             'user_id' => 'int(11) NOT NULL',
             'confirmed' => 'tinyint(4) NULL',
-        ), '');
+        ], '');
 
         $this->createIndex('unique_message_user', 'announcement_user', 'announcement_id,user_id', true);
-//        $this->addForeignKey('fk-message-user', 'announcement_user', 'announcement_id', 'announcement', 'id', 'CASCADE','CASCADE');
+        //$this->addForeignKey('fk-message-user', 'announcement_user', 'announcement_id', 'announcement', 'id', 'CASCADE','CASCADE');
     }
 
     public function down()
@@ -30,14 +30,4 @@ class m171208_130303_initial extends Migration
         return false;
     }
 
-    /*
-      // Use safeUp/safeDown to do migration with transaction
-      public function safeUp()
-      {
-      }
-
-      public function safeDown()
-      {
-      }
-     */
 }
