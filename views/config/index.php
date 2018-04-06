e4b23e0bb52d84a4e07c3172f6b0386d50a6c54f
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-04-07 01:07:08 +0200

    Update index.php

--- views/config/index.php
+++ views/config/index.php
@@ -9,27 +9,29 @@
 /* @var $model \humhub\modules\announcements\models\EditForm */
 
 use yii\widgets\ActiveForm;
-use \yii\helpers\Html;
+use yii\helpers\Html;
 ?>
 
 <div class="panel panel-default">
 
-    <div class="panel-heading"><?= Yii::t('AnnouncementsModule.forms', '<strong>Announcement</strong> module configuration'); ?></div>
+    <div class="panel-heading">
+        <?= Yii::t('AnnouncementsModule.forms', '<strong>Announcement</strong> module configuration'); ?>
+    </div>
 
     <div class="panel-body">
         <?php $form = ActiveForm::begin(); ?>
-        
+
         <h4>
             <?= Yii::t('AnnouncementsModule.forms', 'Global settings'); ?>
         </h4>
-        
+
         <?= $form->field($model, 'notifyCreated')->checkbox(); ?>
         <?= $form->field($model, 'notifyUpdated')->checkbox(); ?>
         <?= $form->field($model, 'notifyClosed')->checkbox(); ?>
         <?= $form->field($model, 'notifyResetStatistics')->checkbox(); ?>
 
-
         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
+
         <?php ActiveForm::end(); ?>
     </div>
 </div>
