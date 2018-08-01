1ebcc483289df07f11fed8e52fa0feb9832e8c21
staxDB <borndavid508@gmail.com>
2018-08-01 13:08:04 +0200

    Removed comments

--- views/announcement/show.php
+++ views/announcement/show.php
@@ -4,26 +4,34 @@ use humhub\modules\announcements\widgets\WallCreateForm;
 use humhub\modules\announcements\permissions\CreateAnnouncement;
 use humhub\modules\stream\widgets\StreamViewer;
 
-echo WallCreateForm::widget([
-    'contentContainer' => $contentContainer,
-    'submitButtonText' => Yii::t('AnnouncementsModule.base', 'Save'),
-]);
 
 $canCreateAnnouncements = $contentContainer->permissionManager->can(new CreateAnnouncement());
+?>
 
-echo StreamViewer::widget([
+<?=  WallCreateForm::widget([
+    'contentContainer' => $contentContainer,
+    'submitButtonText' => Yii::t('AnnouncementsModule.base', 'Save'),
+]); ?>
+
+
+<?php
+ $filters = [
+     'announcement_notAnswered' => Yii::t('AnnouncementsModule.views', 'Not read'),
+     'announcement_mine' => Yii::t('AnnouncementsModule.views', 'Created by me'),
+ ];
+ if(version_compare(Yii::$app->version, '1.3', '>=')) {
+     $filters['topic'] = null;
+ }
+?>
+
+<?= StreamViewer::widget([
     'contentContainer' => $contentContainer,
     'streamAction' => '/announcements/announcement/stream',
     'messageStreamEmpty' => ($canCreateAnnouncements) ?
         Yii::t('AnnouncementsModule.views', '<b>There are no announcements yet!</b><br>Be the first and create one...') :
         Yii::t('AnnouncementsModule.views', '<b>There are no announcements yet!</b>'),
     'messageStreamEmptyCss' => ($canCreateAnnouncements) ? 'placeholder-empty-stream' : '',
-//    'filters' => [
-//        'announcement_notAnswered' => Yii::t('AnnouncementsModule.views', 'Not read'),
-//        'filter_entry_files' => Yii::t('ContentModule.widgets_views_stream', 'Content with attached files'),
-//        'filter_entry_mine' => Yii::t('ContentModule.widgets_views_stream', 'Created by me'),
-//        'filter_entry_archived' => Yii::t('ContentModule.widgets_views_stream', 'Include archived posts'),
-//        'filter_visibility_public' => Yii::t('ContentModule.widgets_views_stream', 'Only public posts'),
-//        'filter_visibility_private' => Yii::t('ContentModule.widgets_views_stream', 'Only private posts')
-//    ]
+    'filters' => $filters
 ]);
+?>
+
