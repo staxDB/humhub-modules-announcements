fc2b06a8a45fc338a31d7c3566bdc2b957f9d592
Xaro <6691309+xaro1@users.noreply.github.com>
2018-08-03 11:57:04 +0200

    French translation

--- messages/fr/models.php
+++ messages/fr/models.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'Not read by me' => '',
-    'Old Announcement' => '',
-    'Read' => '',
+    'Not read by me' => 'Non lu pas moi',
+    'Old Announcement' => 'Ancienne annonce',
+    'Read' => 'Lu',
     'Message' => 'Message',
 ];
