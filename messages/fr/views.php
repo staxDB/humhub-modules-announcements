656bbe6d9aae82831078a977f479ffe16838b4b7
Xaro <6691309+xaro1@users.noreply.github.com>
2018-08-03 11:57:04 +0200

    French translation

--- messages/fr/views.php
+++ messages/fr/views.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'Created by me' => '',
-    'Not read' => '',
+    'Created by me' => 'Créé par moi',
+    'Not read' => 'Non lu',
     '<b>There are no announcements yet!</b>' => '<b>Il n\'y a aucune annonce importante.</b>',
     '<b>There are no announcements yet!</b><br>Be the first and create one...' => '<b>Il n\'y a aucune annonce importante.</b><br>Soyez le premier à en publier une...<br>',
     'Save' => 'Publier',
