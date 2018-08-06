5eda93b2e8548eb0f40953a22020ee154b2ed16a
Xaro <6691309+xaro1@users.noreply.github.com>
2018-08-06 15:06:55 +0200

    French translation typo

--- messages/fr/views.php
+++ messages/fr/views.php
@@ -2,7 +2,7 @@
 
 return [
     'Created by me' => 'Créée par moi',
-    'Not read' => 'Non lue',
+    'Not read' => 'Non lu',
     '<b>There are no announcements yet!</b>' => '<b>Il n\'y a aucune annonce importante.</b>',
     '<b>There are no announcements yet!</b><br>Be the first and create one...' => '<b>Il n\'y a aucune annonce importante.</b><br>Soyez le premier à en publier une...<br>',
     'Save' => 'Publier',
