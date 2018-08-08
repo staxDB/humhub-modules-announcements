4ebb500e1138b7cf6f860b1e82463528778ec215
Xaro <6691309+xaro1@users.noreply.github.com>
2018-08-08 08:31:52 +0200

    French translation

--- messages/fr/controller.php
+++ messages/fr/controller.php
@@ -17,7 +17,7 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Users who read this <strong>{title}</strong>' => 'Les membres qui ont lu cette <strong>{title}</strong>',
+    'Users who read this <strong>{title}</strong>' => 'Les membres qui ont lu <strong>{title}</strong>',
     'Access denied!' => 'Accès refusé.',
     'Announcement not found!' => 'Annonce non trouvée.',
     'Could not load Announcement!' => 'Impossible d\'afficher l\'annonce.',
