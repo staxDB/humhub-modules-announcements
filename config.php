111483220866ea90b3a06fda67a6e80f6a829445
staxDB <borndavid508@gmail.com>
2018-08-30 11:32:27 +0200

    Updated for version release

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
+use humhub\modules\announcements\Events;
 use humhub\modules\announcements\Module;
 use humhub\modules\space\widgets\Menu;
 use humhub\modules\user\models\User;
@@ -11,16 +12,16 @@ use humhub\modules\stream\widgets\WallStreamFilterNavigation;
 
 return [
     'id' => 'announcements',
-    'class' => 'humhub\modules\announcements\Module',
+    'class' => Module::class,
     'namespace' => 'humhub\modules\announcements',
     'events' => [
-        ['class' => WallEntryControls::class, 'event' => WallEntryControls::EVENT_INIT, 'callback' => ['humhub\modules\announcements\Events', 'onWallEntryControlsInit']],
-        ['class' => User::class, 'event' => User::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\announcements\Events', 'onUserDelete']],
-        ['class' => Menu::class, 'event' => Menu::EVENT_INIT, 'callback' => ['humhub\modules\announcements\Events', 'onSpaceMenuInit']],
-        ['class' => Membership::class, 'event' => Membership::EVENT_MEMBER_ADDED, 'callback' => ['humhub\modules\announcements\Events', 'onMemberAdded']],
-        ['class' => Membership::class, 'event' => Membership::EVENT_MEMBER_REMOVED, 'callback' => ['humhub\modules\announcements\Events', 'onMemberRemoved']],
-        ['class' => IntegrityController::class, 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => ['humhub\modules\announcements\Events', 'onIntegrityCheck']],
-        ['class' => WallStreamQuery::class, 'event' =>  WallStreamQuery::EVENT_BEFORE_FILTER, 'callback' => ['humhub\modules\announcements\Events', 'onStreamFilterBeforeFilter']],
-        ['class' => WallStreamFilterNavigation::class, 'event' =>  WallStreamFilterNavigation::EVENT_BEFORE_RUN, 'callback' => ['humhub\modules\announcements\Events', 'onStreamFilterBeforeRun']],
+        ['class' => WallEntryControls::class, 'event' => WallEntryControls::EVENT_INIT, 'callback' => [Events::class, 'onWallEntryControlsInit']],
+        ['class' => User::class, 'event' => User::EVENT_BEFORE_DELETE, 'callback' => [Events::class, 'onUserDelete']],
+        ['class' => Menu::class, 'event' => Menu::EVENT_INIT, 'callback' => [Events::class, 'onSpaceMenuInit']],
+        ['class' => Membership::class, 'event' => Membership::EVENT_MEMBER_ADDED, 'callback' => [Events::class, 'onMemberAdded']],
+        ['class' => Membership::class, 'event' => Membership::EVENT_MEMBER_REMOVED, 'callback' => [Events::class, 'onMemberRemoved']],
+        ['class' => IntegrityController::class, 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => [Events::class, 'onIntegrityCheck']],
+        ['class' => WallStreamQuery::class, 'event' =>  WallStreamQuery::EVENT_BEFORE_FILTER, 'callback' => [Events::class, 'onStreamFilterBeforeFilter']],
+        ['class' => WallStreamFilterNavigation::class, 'event' =>  WallStreamFilterNavigation::EVENT_BEFORE_RUN, 'callback' => [Events::class, 'onStreamFilterBeforeRun']],
     ],
 ];
