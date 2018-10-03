5155262e9cd30dbc3b6ce1448d31311105b79e66
staxDB <borndavid508@gmail.com>
2018-10-03 17:47:05 +0200

    Last changes for update v0.3.7

--- views/announcement/edit.php
+++ views/announcement/edit.php
@@ -2,6 +2,8 @@
 
 use humhub\modules\ui\form\widgets\ActiveForm;
 use humhub\modules\content\widgets\richtext\RichTextField;
+use humhub\modules\file\widgets\UploadButton;
+use humhub\modules\file\widgets\FilePreview;
 
 /* @var $announcement \humhub\modules\announcements\models\Announcement */
 /* @var $user \humhub\modules\user\models\User */
@@ -22,18 +24,42 @@ use humhub\modules\content\widgets\richtext\RichTextField;
         'placeholder' => Yii::t('AnnouncementsModule.base', 'Edit your message')
     ])->label(false) ?>
 
-    <a href="#" class="btn btn-primary"
-       data-action-click="editSubmit" data-action-submit
-       data-action-url="<?= $announcement->content->container->createUrl('/announcements/announcement/edit', ['id' => $announcement->id]) ?>"
-       data-ui-loader>
-        <?= Yii::t('AnnouncementsModule.views', 'Save') ?>
-    </a>
+    <div class="comment-buttons">
+        <?= UploadButton::widget([
+            'id' => 'announcement_upload_' . $announcement->id,
+            'model' => $announcement,
+            'dropZone' => '#announcement_' . $announcement->id,
+            'preview' => '#announcement_upload_preview_' . $announcement->id,
+            'progress' => '#announcement_upload_progress_' . $announcement->id,
+            'max' => Yii::$app->getModule('content')->maxAttachedFiles
+        ]);
+        ?>
+    </div>
 
-    <a href="#" class="btn btn-danger"
-       data-action-click="editCancel"
-       data-action-url="<?= $announcement->content->container->createUrl('/announcements/announcement/reload', ['id' => $announcement->id]) ?>"
-       data-ui-loader>
-        <?= Yii::t('AnnouncementsModule.views', "Cancel") ?>
-    </a>
+    <div id="announcement_upload_progress_<?= $announcement->id ?>" style="display:none; margin:10px 0;"></div>
+
+    <?= FilePreview::widget([
+        'id' => 'announcement_upload_preview_' . $announcement->id,
+        'options' => ['style' => 'margin-top:10px'],
+        'model' => $announcement,
+        'edit' => true
+    ]);
+    ?>
+
+    <div class="edit_buttons">
+        <a href="#" class="btn btn-default btn-sm btn-comment-submit"
+           data-action-click="editSubmit"
+           data-action-url="<?= $announcement->content->container->createUrl('/announcements/announcement/edit', ['id' => $announcement->id]) ?>"
+           data-action-submit
+           data-ui-loader>
+            <?= Yii::t('AnnouncementsModule.views', 'Save') ?>
+        </a>
+        <a href="#" class="btn btn-danger btn-sm btn-comment-submit"
+           data-action-click="editCancel"
+           data-action-url="<?= $announcement->content->container->createUrl('/announcements/announcement/reload', ['id' => $announcement->id]) ?>"
+           data-ui-loader>
+            <?= Yii::t('AnnouncementsModule.views', "Cancel") ?>
+        </a>
+    </div>
     <?php ActiveForm::end(); ?>
 </div>
